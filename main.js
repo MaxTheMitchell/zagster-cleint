32509cee990b1f8e6728b0840bbf7618991ffaf3
Max Mitchell <rat2043@gmail.com>
2018-11-05 15:23:22 -0800

    Added to main.js to replace "TO DO" with data

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 $(updateVeiw)
 
+const Base_URL = "https://zagster-service.herokuapp.com"
+
 function updateVeiw(){
     alert("CLICK 'OK' FOR FREE I-PHONE!!!")
+    $.getJSON(Base_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+    numberOfRides = data.count
+    $("h2#rideCount").html(numberOfRides)
 }
