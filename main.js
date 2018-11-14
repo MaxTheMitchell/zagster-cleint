3696512eaee71cfe3ae0ddb69e6c3f81a7cc4144
Max Mitchell <rat2043@gmail.com>
2018-11-13 23:03:32 -0800

    added modifications to make it look like I want my data visualisation to look like

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(updateVeiw)
 const Base_URL = "https://zagster-service.herokuapp.com"
 
 function updateVeiw(){
-    alert("CLICK 'OK' FOR FREE I-PHONE!!!")
     $.getJSON(Base_URL + "/rides/count" , updateRideCount)
 }
 
